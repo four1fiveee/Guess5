922d62a0bbab60b872f860210b5a670f18d0f5f3
four1fiveee <four1fiveee@gmail.com>
2025-08-08 16:00:40 -0400

    remove dup logo

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,20 +6,16 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { useMemo } from 'react';
-import { useRouter } from 'next/router';
-import LogoHeader from '../components/LogoHeader';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => process.env.NEXT_PUBLIC_SOLANA_NETWORK || 'https://api.devnet.solana.com', []);
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
-  const router = useRouter();
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          {router.pathname !== '/' && <LogoHeader />}
           <Component {...pageProps} />
         </WalletModalProvider>
       </WalletProvider>
