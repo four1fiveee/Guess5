19b9536074f2089764b0250f0adc303392a4af5c
four1fiveee <four1fiveee@gmail.com>
2025-09-09 22:22:14 -0400

    IDL fix:

--- backend/src/types/guess5.ts
+++ backend/src/types/guess5.ts
@@ -350,6 +350,32 @@ export const IDL = {
           }
         ]
       }
+    },
+    {
+      "name": "result",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "Player1"
+          },
+          {
+            "name": "Player2"
+          },
+          {
+            "name": "WinnerTie"
+          },
+          {
+            "name": "LosingTie"
+          },
+          {
+            "name": "Timeout"
+          },
+          {
+            "name": "Error"
+          }
+        ]
+      }
     }
   ],
   "errors": [
