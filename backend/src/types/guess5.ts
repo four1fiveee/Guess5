f6e7147feb5d4c5ffaa3934d848fb0d390fbfb7b
four1fiveee <four1fiveee@gmail.com>
2025-10-23 23:12:41 -0400

    Update smart contract to use correct program ID from provided keypair

--- backend/src/types/guess5.ts
+++ backend/src/types/guess5.ts
@@ -1,5 +1,5 @@
 export const IDL = {
-  "address": "ASLA3yCccjSoMAxoYBciM5vqdCZKcedd2QkbVWtjQEL4",
+  "address": "CdfA7r3o4Tne1cv1GGwrvHvBPdNfDgqW3mF5hHXdFXy3",
   "metadata": {
     "name": "guess5_escrow",
     "version": "0.1.0",
